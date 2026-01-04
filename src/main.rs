30f0f9b20e662fd13a30c30f4fe0fc9e13e28d00
Matt Thorning <matthewthorning@gmail.com>
2026-01-04 12:20:12 +0000

    print contents of file to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,86 @@
+use anyhow::Result;
 use clap::{Args, Parser, Subcommand};
+use env_logger;
 use std::fs::{OpenOptions, metadata};
-use std::io::{self, Read, Write};
+use std::io::{Read, Write};
 
-const TASKS_FILE: &str = "tasks.txt";
+const TASKS_FILE: &str = "tasks.md";
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[command(subcommand)]
-    command: Command,
+  #[command(subcommand)]
+  command: Command,
 }
 
 #[derive(Subcommand)]
 enum Command {
-    Task(TaskArgs),
-    // Timer(TimerArgs),
-    // Note(NoteArgs),
-    Status,
+  Task(TaskArgs),
+  // Timer(TimerArgs),
+  // Note(NoteArgs),
+  Status,
 }
 
 #[derive(Args)]
 struct TaskArgs {
-    #[arg(short, long, value_name = "TASK")]
-    new: String,
+  #[command(subcommand)]
+  command: TaskCommand,
 }
 
-fn main() {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        Command::Task(args) => {
-            if args.new != "" {
-                let result = write_task_to_file(&args.new);
-                match result {
-                    Ok(_) => println!("Task added: {:?}", args.new),
-                    Err(err) => panic!("Error: {:?}", err),
-                }
-            }
-        }
-        _ => println!("Not supported"),
-    }
+#[derive(Subcommand)]
+enum TaskCommand {
+  Add {
+    #[arg(value_name = "TASK")]
+    task: String,
+  },
+  List,
 }
 
-fn write_task_to_file(task: &str) -> io::Result<()> {
-    let needs_newline = match metadata(TASKS_FILE) {
-        Ok(meta) => meta.len() > 0,
-        Err(_) => false,
-    };
+fn main() -> Result<()> {
+  env_logger::init();
+  let cli = Cli::parse();
 
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(TASKS_FILE)?;
+  match &cli.command {
+    Command::Task(task_cmd) => match &task_cmd.command {
+      TaskCommand::Add { task } => {
+        write_task_to_file(&task)?;
+        println!("Task added: {}", task);
+      }
+      TaskCommand::List => {
+        list_tasks()?;
+      }
+    },
+    Command::Status => println!("I don't know yet"),
+  }
+  Ok(())
+}
 
-    if needs_newline {
-        file.write_all(b"\n")?;
-    }
+fn write_task_to_file(task: &str) -> Result<()> {
+  let needs_newline = match metadata(TASKS_FILE) {
+    Ok(meta) => meta.len() > 0,
+    Err(_) => false,
+  };
 
-    file.write_all(task.as_bytes())?;
+  let mut file = OpenOptions::new()
+    .append(true)
+    .create(true)
+    .open(TASKS_FILE)?;
 
-    Ok(())
+  let new_line = if needs_newline { "\n" } else { "" };
+  let task_str = format!("{}- [ ] {}", new_line, task);
+
+  file.write_all(task_str.as_bytes())?;
+
+  Ok(())
+}
+
+fn list_tasks() -> Result<()> {
+  let mut file = OpenOptions::new().read(true).open(TASKS_FILE)?;
+
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+
+  println!("{}", contents);
+
+  Ok(())
 }
---
+++ tasks.md
@@ -0,0 +1,6 @@
+- [ ] Test 1
+- [ ] Test 2
+- [ ] Test 3
+Not a task
+- this is a note
+- [ ] Test 3
